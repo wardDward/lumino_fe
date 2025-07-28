9afd03d7d2106e6f1a5671d7448faced26235977
wardDward <edward.taligatos03@gmail.com>
2025-07-28 21:26:01 +0800

    feat: login UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { Template as RegularUserTemplate } from './pages/regular-user/template/template';
+import { Login } from './pages/public/login/login';
 
 export const routes: Routes = [
     {
+        path: 'login',
+        title: 'Login',
+        component: Login
+    },
+    {
         path: '',
+        title: 'Lumino',
         component: RegularUserTemplate,
         loadChildren: () => import('./pages/regular-user/regular-user.module').then((m) => m.RegularUserModule)
         
