4555a49113990babcaefc79499481b7e773fe53f
wardDward <edward.taligatos03@gmail.com>
2025-08-02 19:28:02 +0800

    chore: fix login validation

--- src/app/pages/public/auth/login/login.component.ts
+++ src/app/pages/public/auth/login/login.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { AuthService } from './../../../../services/auth/auth.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ValidationComponent } from '../../../../common/validation/validation.component';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, ReactiveFormsModule, ValidationComponent],
+  imports: [CommonModule,RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -18,19 +18,13 @@ export class LoginComponent {
     password: new FormControl('', [Validators.required ,Validators.maxLength(100)])
   })
 
-  get email() {
-    return this.form.get("email")
-  }
-
-  get password(){
-    return this.form.get("password")
-  }
-   
   login(){
-// disable the submit button if not filled up the input
     console.log('login test')
     const form = this.form.getRawValue();
     this.authService.login(form)
+
+    // if error just set message to invalid creds
+
   }
 }
 
