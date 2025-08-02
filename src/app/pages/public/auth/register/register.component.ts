b2e8f7e909cd8fb947faced4e232dd08f9c0be05
wardDward <edward.taligatos03@gmail.com>
2025-08-02 17:07:51 +0800

    feat: registration validation

--- src/app/pages/public/auth/register/register.component.ts
+++ src/app/pages/public/auth/register/register.component.ts
@@ -1,12 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-
+import { passwordMismatchValidator } from '../../../../utils/passwordMismatch.validator';
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [CommonModule ,RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  
+
+  ngOnInit(){
+    console.log(this.form)
+  }
+  form = new FormGroup({
+    first_name: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+    last_name: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+    middle_name: new FormControl('', [Validators.maxLength(100)]),
+    suffix: new FormControl('', [Validators.maxLength(100)]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(100)]),
+    contact: new FormControl('', [Validators.required, Validators.maxLength(13)]),
+    password: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+    password_confirmation: new FormControl('', [Validators.required, Validators.maxLength(100)])
+  }, {validators: passwordMismatchValidator})
+
+  register() {
+    console.log(this.form)
+  }
 
 }
+
+
+
+export interface User {
+  first_name: string;
+  last_name: string;
+  middlename?: string;
+  suffix?: string;
+  email: string;
+  contact: string
+  password: string;
+  password_confirmation: string;
+}
+
