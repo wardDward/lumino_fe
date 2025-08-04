b03c39bae45c91b013ab7e3cccc28993a046bfd6
wardDward <edward.taligatos03@gmail.com>
2025-08-04 13:55:59 +0800

    chore: login service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-    api:string = `${environment.api}/api/auth`
-
+    api:string = `${environment.api}`
+    httpOptions = {
+      withCredentials: true,
+    }
     constructor(private http: HttpClient){}
 
-    login(payload:any){
-      return this.http.post(`${this.api}/login`, {email:payload.email, password:payload.password}, {observe: 'response'})
-    }
+    //add interface for params
+   login(payload: any) {
+  return this.http.get(`${this.api}/sanctum/csrf-cookie`, this.httpOptions).pipe(
+    switchMap(() => {
+      return this.http.post(
+        `${this.api}/api/auth/login`,
+        payload,
+        {
+          ...this.httpOptions,
+          observe: 'response',
+        }
+      );
+    })
+  );
+}
+
 
 }
