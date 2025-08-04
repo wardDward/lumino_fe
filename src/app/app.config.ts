2156d672e6d205548eb13a729d3550908c185316
wardDward <edward.taligatos03@gmail.com>
2025-08-04 13:55:28 +0800

    feat: auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
+import { authInterceptor } from './interceptor/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',     
+        headerName: 'X-XSRF-TOKEN'
+      }),withInterceptors([authInterceptor])),
     provideRouter(routes)
   ]
 };
---
+++ src/app/interceptor/auth/auth-interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn, HttpXsrfTokenExtractor } from '@angular/common/http';
+import { inject } from '@angular/core';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+   const extractor = inject(HttpXsrfTokenExtractor);
+  const token = extractor.getToken();
+  const headerName = 'X-XSRF-TOKEN';
+
+  if (req.method === 'GET' || req.method === 'HEAD') return next(req);
+  if (token && !req.headers.has(headerName)) {
+    req = req.clone({ setHeaders: { [headerName]: token } });
+  }
+  return next(req);
+};
